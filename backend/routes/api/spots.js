b458142f972c6e47be27921147c34c8754bb080e
Yasha Yang <yashayang@Yashas-MacBook-Air.local>
2022-08-30 15:49:00 -0700

    Get All Spot Endpoint with eager loading

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,17 +1,43 @@
-
-
-
-
 const express = require('express');
 const router = express.Router();
 
-const { Spot, SpotImages, Review } = require('../../db/models');
+const { Spot, SpotImage, Review, sequelize } = require('../../db/models');
 
 router.get('/', async (req, res, next) => {
   const allSpots = await Spot.findAll();
-  const avgRating
+  // let spotArray = allSpots.map( async (spot) => {
+  // })
+  let spotArray = [];
+  for (let i = 0; i < allSpots.length; i++) {
+    let spotId = allSpots[i].id;
+      const avgRating = await Spot.findByPk(spotId, {
+        attributes: {
+          include: [
+            [
+              sequelize.fn('AVG', sequelize.col('Reviews.stars')),
+              'avgRating'
+            ]
+          ]
+        },
+        include: {
+          model: Review,
+          attributes: []
+        }
+      })
+      const avgRatingObj = avgRating.toJSON();
+      const previewImageUrl = await SpotImage.findByPk(spotId, {
+        where: { preview: true },
+        attributes: ['url']
+      })
+      console.log(previewImageUrl)
+      avgRatingObj["prevewImage"] = previewImageUrl.url
+      // res.send(avgRatingObj)
+      // res.send(avgRating.toJSON().prevewImage)
+      spotArray.push(avgRatingObj)
+    }
 
-  res.json(allSpots)
+
+    res.json({'Spots': spotArray})
 })
 
 router.get('/current', async (req, res, next) => {
