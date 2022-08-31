d958669e5ee82ebc06bd7e36fd20bafc1f4b2489
Yasha Yang <yashayang@Yashas-MacBook-Air.local>
2022-08-30 21:37:14 -0700

    Edit a Spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -173,6 +173,46 @@ router.post('/:spotId/images', async (req, res, next) => {
   res.json(result)
 })
 
+//Edit a Spot
+router.put('/:spotId', async (req, res, next) => {
+
+  const currSpotId = parseInt(req.params.spotId);
+  const currSpot = await Spot.findByPk(currSpotId);
+  if (!currSpot) {
+    res.status(404)
+    .json({
+      "message": "Spot couldn't be found",
+      "statusCode": 404
+    })
+  }
+
+  try {
+
+  const { address, city, state, country, lat, lng, name, description, price } = req.body;
+  currSpot.update({ address, city, state, country, lat, lng, name, description, price });
+  res.json(currSpot);
+
+  } catch (err) {
+    res.json({
+      "message": "Validation Error",
+      "statusCode": 400,
+      "errors": {
+        "address": "Street address is required",
+        "city": "City is required",
+        "state": "State is required",
+        "country": "Country is required",
+        "lat": "Latitude is not valid",
+        "lng": "Longitude is not valid",
+        "name": "Name must be less than 50 characters",
+        "description": "Description is required",
+        "price": "Price per day is required"
+      }
+    })
+  }
+
+})
+
+
 module.exports = router;
 
 
