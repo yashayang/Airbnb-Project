f39820a6705b530759d97dd10a93662238d57f98
Yasha Yang <yashayang@Yashas-MacBook-Air.local>
2022-08-31 11:17:52 -0700

    Add an Image to a Review based on the Review's id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { User, Spot, SpotImage, Review, sequelize } = require('../../db/models');
+const { User, Spot, SpotImage, Review, sequelize, ReviewImage } = require('../../db/models');
 
 //Get all Reviews of the Current User
 router.get('/:userId', async (req, res, next) => {
@@ -33,6 +33,41 @@ router.get('/:userId', async (req, res, next) => {
 
 })
 
+//Add an Image to a Review based on the Review's id
+router.post('/:reviewId/images', async (req, res, next) => {
+  const reviewId = parseInt(req.params.reviewId);
+  const { url } = req.body;
+
+  const currReview = await Review.findByPk(reviewId);
+  if (!currReview) {
+    res.status(404)
+       .json({
+       "message": "Review couldn't be found",
+       "statusCode": 404
+       })
+  }
+
+  const images = await ReviewImage.findAll({ where: { reviewId: reviewId } })
+  if (images.length >= 10) {
+    res.status(403)
+       .json({
+       "message": "Maximum number of images for this resource was reached",
+       "statusCode": 403
+       })
+  }
+
+  const newImage = await ReviewImage.create({
+    reviewId: reviewId,
+    url: url
+  })
+
+  const result = await ReviewImage.findOne({
+    where: {url: url},
+    attributes: ['id', 'url']
+  })
+
+  res.json(result)
+})
 
 
 module.exports = router;
