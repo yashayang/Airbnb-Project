0a1fa5b5a9baaae6a5b08b83ee85bfaec33f420b
Yasha Yang <yashayang@Yashas-MacBook-Air.local>
2022-08-31 19:03:41 -0700

    Delete a Booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -102,7 +102,39 @@ router.put('/:bookingId', async (req, res, next) => {
 
 })
 
+//Delete a Booking
+router.delete('/:bookingId', async(req, res, next) => {
+  const bookingId = parseInt(req.params.bookingId);
+  const currBooking = await Booking.findByPk(bookingId);
+  if (!currBooking) {
+    res.status(404)
+    .json({
+      "message": "Booking couldn't be found",
+      "statusCode": 404
+    })
+  }
 
+  let q = new Date();
+  let m = q.getMonth()+1;
+  let d = q.getUTCDate();
+  let y = q.getFullYear();
+  const today = `${y}-${m}-${d}`;
+  console.log(currBooking.toJSON().startDate)
+  if (currBooking.toJSON().startDate <= today) {
+    res.status(403)
+    .json({
+      "message": "Bookings that have been started can't be deleted",
+      "statusCode": 403
+    })
+  }
+
+  currBooking.destroy();
+  res.json({
+    "message": "Successfully deleted",
+    "statusCode": 200
+  })
+
+})
 
 
 
